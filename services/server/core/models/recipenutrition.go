6bb699ea719d07572c757e0c4cf162b7de8b73da
ilovelili <route666@live.cn>
2019-01-30 12:50:34 +0800

    simple refactoring

--- services/server/core/models/recipenutrition.go
+++ services/server/core/models/recipenutrition.go
@@ -5,7 +5,7 @@ type RecipeNutrition struct {
 	ID           int64   `dapper:"id,primarykey,autoincrement,table=recipe_nutritions" csv:"-"`
 	Recipe       string  `dapper:"recipe" csv:"recipe"`
 	Carbohydrate float64 `dapper:"carbohydrate" csv:"carbohydrate"`
-	DietaryFiber float64 `dapper:"dietaryfiber" csv:"dietaryfiber"`
+	Dietaryfiber float64 `dapper:"dietaryfiber" csv:"dietaryfiber"`
 	Protein      float64 `dapper:"protein" csv:"protein"`
 	Fat          float64 `dapper:"fat" csv:"fat"`
 	Heat         float64 `dapper:"heat" csv:"heat"`
