aef0d9180d1ea7e45e5abb5efd0803711ffd9027
ilovelili <route666@live.cn>
2020-01-10 10:25:46 +0800

    fix unit amount bug

--- services/server/core/models/recipe.go
+++ services/server/core/models/recipe.go
@@ -2,10 +2,10 @@ package models
 
 // Recipe recipe entry
 type Recipe struct {
-	ID             int64   `dapper:"id,primarykey,autoincrement,table=recipes" csv:"-"`
-	Name           string  `dapper:"name" csv:"菜品名称"`
-	Ingredient     int64   `dapper:"ingredient_id" csv:"-"`
-	IngredientName string  `dapper:"-" csv:"原料名称"`
-	UnitAmount     float64 `dapper:"unit_amount" csv:"-"`
-	CreatedBy      string  `dapper:"created_by" csv:"-"`
+	ID             int64    `dapper:"id,primarykey,autoincrement,table=recipes" csv:"-"`
+	Name           string   `dapper:"name" csv:"菜品名称"`
+	Ingredient     int64    `dapper:"ingredient_id" csv:"-"`
+	IngredientName string   `dapper:"-" csv:"原料名称"`
+	UnitAmount     *float64 `dapper:"unit_amount" csv:"-"`
+	CreatedBy      string   `dapper:"created_by" csv:"-"`
 }
